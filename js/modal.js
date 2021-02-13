be0e229cb04bef354743aef1efe5eda88b83b576
Adeline Claire MOREL <adelinemorel72@gmail.com>
2021-02-13 09:27:08 +0100

    not allowing to close modal if required entries are empty and adding a constant to verify the validity of email

--- js/modal.js
+++ js/modal.js
@@ -11,7 +11,8 @@ function editNav() {
 const modalBg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const closeModal = document.querySelector("#close");
+const closeModal = document.querySelector("#close"); /*Cette constante permet de cibler l'élément du DOM ayant l'id "close" (la croix qui ferme la modale)*/
+const email = document.querySelector("#email"); /*Cette constante permet de cibler l'élément du DOM ayant l'id "email"*/
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -25,3 +26,12 @@ function launchModal() {
 closeModal.addEventListener('click', event => {
   modalBg.style.display = "none";
 })
+/*Cette fonction modifie le style de l'élément ayant la classe ".bground"
+(le parent le plus haut de la modale, qui contient tous les éléments enfants de celle-ci)
+et passe sa propriété display en none, lorsque l'utilisateur clique sur la croix (qui a l'id "close").
+Cela fonctionne avec un listener de l'événement "click"*/
+
+// check entries validity
+let emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+/*Ici j'utilise du Regex pour respecter la spécification html5 pour le format de l'adresse mail de l'utilisateur
+Note : "2@2" et "2@2.2" fonctionnent, cela mérite d'être revérifié.*/
